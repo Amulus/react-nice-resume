1d2abcd950c64765ab21a6bf27764b0445dbc40b
Axel <axel.murat@laposte.net>
2021-02-08 15:29:40 +0100

    a

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
             <img className="profile-pic"  src={profilepic} alt="Nordic Giant Profile Pic" />
          </div>
          <div className="nine columns main-col">
-            <h2>à propos</h2>
+            <h2>À propos</h2>
 
             <p>{bio}</p>
             <div className="row">
