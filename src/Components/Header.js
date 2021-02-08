36c167332ed7e3a2041c2e6103a74f940ae3434e
Axel <axel.murat@laposte.net>
2021-02-08 15:58:48 +0100

    final fix + projet

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -39,7 +39,6 @@ class Header extends Component {
             <hr />
             <ul className="social">
                <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
             </ul>
          </div>
       </div>
